c9338ce55822886e5ec58b6cce41de4b5d4f5083
Tanner <tanner2379@live.com>
2021-09-25 10:48:14 -0700

    done

--- src/hoc/Layouts/Layout.js
+++ src/hoc/Layouts/Layout.js
@@ -1,42 +1,43 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext } from "react";
 
-import Aux from '../Aux/Aux';
-import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
-import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
-import Flash from '../../components/UI/Flash/Flash';
+import Aux from "../Aux/Aux";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
+import Flash from "../../components/UI/Flash/Flash";
 
-import { FlashContext }  from '../../contexts/FlashContext'
+import { FlashContext } from "../../contexts/FlashContext";
 
-import classes from './Layout.module.css'
+import classes from "./Layout.module.css";
 
-const Layout = props => {
+const Layout = (props) => {
   const [flash, setFlash] = useContext(FlashContext);
   const [sideDrawerVisible, setSideDrawerVisible] = useState(false);
 
   const sideDrawerClosedHandler = () => {
     setSideDrawerVisible(false);
-  }
+  };
 
   const sideDrawerToggleHandler = () => {
     setSideDrawerVisible(!sideDrawerVisible);
-  }
+  };
 
   const closeFlash = () => {
-    setFlash({messages: [], visible: false, type: null})
-  }
+    setFlash({ messages: [], visible: false, type: null });
+  };
 
   return (
     <Aux>
       <Toolbar {...props} drawerToggleClicked={sideDrawerToggleHandler} />
-      <SideDrawer {...props} open={sideDrawerVisible} closed={sideDrawerClosedHandler} />
-        {flash.visible
-          ? <Flash close={closeFlash} messages={flash.messages} type={flash.type} />
-          : null
-        }
-        <div className={classes.Content}>
-          {props.children}
-        </div>
-      
+      <SideDrawer
+        {...props}
+        open={sideDrawerVisible}
+        closed={sideDrawerClosedHandler}
+      />
+      {flash.visible ? (
+        <Flash close={closeFlash} messages={flash.messages} type={flash.type} />
+      ) : null}
+      <div className={classes.Content}>{props.children}</div>
+
       {/* <div className={classes.Footer}>
         <Footer />
       </div> */}
@@ -45,3 +46,4 @@ const Layout = props => {
 };
 
 export default Layout;
+
